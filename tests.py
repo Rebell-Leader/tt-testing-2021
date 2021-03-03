b4e19bbfb75b70dee0fe86de4a65c64f76cb3340
Airat Valiev <33163488+Rebell-Leader@users.noreply.github.com>
2021-03-03 17:25:22 +0300

    Add tests for str

--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
 import pytest
 
-
-def test_str_1():
-    assert (1, 2, 3) == (1, 2, 3)
+@pytest.mark.parametrize("test_input,expected", [('a', '%a%'), ('%a', '%%a'), ('a%', '%a%')])
+def test_str_1(test_input, expected):
+    got_result = test_input.center(3, '%')
+    assert got_result == expected
 
 
 def test_str_2():
-    pass
+    string_with_spaces = '\n    \n \r'
+    string_without_spaces = 'string'
+    TrueValue = string_with_spaces.isspace()
+    FalseValue = string_without_spaces.isspace()
+    assert TrueValue == True and FalseValue == False
 
 
 def test_str_3():
-    pass
+    string = 'john doe'
+    reference_string = 'John Doe'
+    capitalized_string = string.split()[0].capitalize() + ' ' + string.split()[1].capitalize()
+    assert capitalized_string == reference_string
 
 
 def test_list_1():
